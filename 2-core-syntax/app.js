b4146780ff8ca1c1d524049255f0070fdf726054
강명구 <mg5566@hanmail.net>
2022-03-28 18:50:52 +0900

    feat: add tuple type

--- 2-core-syntax/app.js
+++ 2-core-syntax/app.js
@@ -1,8 +1,13 @@
 var person = {
     name: "Kang",
     age: 30,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
+person.role.push("admin");
+// person.role[1] = 10; // error
+person.role = [5, "admin"];
+// person.role = [4, "admin", "user"];  // error
 // let favoriteActivities: any[];
 // favoriteActivities = ["Sport", 1]
 var favoriteActivities;
--- 2-core-syntax/app.ts
+++ 2-core-syntax/app.ts
@@ -1,8 +1,19 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Kang",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+person.role.push("admin");
+// person.role[1] = 10; // error
+person.role = [5, "admin"];
+// person.role = [4, "admin", "user"];  // error
 
 // let favoriteActivities: any[];
 // favoriteActivities = ["Sport", 1]
