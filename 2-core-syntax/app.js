8c56c64a2c8c4e7eef5130280a487c38c31dccb8
강명구 <mg5566@hanmail.net>
2022-03-28 16:54:44 +0900

    feat: add Object type

--- 2-core-syntax/app.js
+++ 2-core-syntax/app.js
@@ -1,17 +1,5 @@
-console.log("loading");
-var add = function (num1, num2, showResult, phrase) {
-    var result = num1 + num2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+var person = {
+    name: "Kang",
+    age: 30
 };
-// const number1 = "5";  // type error
-var number1 = 5;
-var number2 = 2.5;
-var printResult = true;
-var resultPhrase = "Result is: ";
-var result = add(number1, number2, printResult, resultPhrase);
-// console.log(result);
+console.log(person);
