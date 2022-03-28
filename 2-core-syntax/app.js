3a792a788cd5d1e40f0e22dd24039ab36c80b0fb
강명구 <mg5566@hanmail.net>
2022-03-28 18:37:17 +0900

    feat: add array type

--- 2-core-syntax/app.js
+++ 2-core-syntax/app.js
@@ -1,5 +1,14 @@
 var person = {
     name: "Kang",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
 };
+// let favoriteActivities: any[];
+// favoriteActivities = ["Sport", 1]
+var favoriteActivities;
+favoriteActivities = ["Sport"];
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- 2-core-syntax/app.ts
+++ 2-core-syntax/app.ts
@@ -1,6 +1,16 @@
-const person: { name: string; age: number } = {
+const person = {
   name: "Kang",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+// let favoriteActivities: any[];
+// favoriteActivities = ["Sport", 1]
+let favoriteActivities: string[];
+favoriteActivities = ["Sport"];
+
 console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
