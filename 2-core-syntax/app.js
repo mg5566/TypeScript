cd1b1690bb3157fb1a92be7e207fe7bdd8e2ed80
강명구 <mg5566@hanmail.net>
2022-03-28 16:45:35 +0900

    feat: add basic types

--- 2-core-syntax/app.js
+++ 2-core-syntax/app.js
@@ -1,8 +1,17 @@
 console.log("loading");
-var add = function (num1, num2) {
-    return num1 + num2;
+var add = function (num1, num2, showResult, phrase) {
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 };
 // const number1 = "5";  // type error
 var number1 = 5;
 var number2 = 2.5;
-add(number1, number2);
+var printResult = true;
+var resultPhrase = "Result is: ";
+var result = add(number1, number2, printResult, resultPhrase);
+// console.log(result);
