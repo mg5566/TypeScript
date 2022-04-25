0873e566002d4311828c3f3bed06d15b6a58d0fe
강명구 <mg5566@hanmail.net>
2022-04-25 13:02:03 +0900

    feat: add new decorator

--- 8-decorators/src/app.ts
+++ 8-decorators/src/app.ts
@@ -34,3 +34,55 @@ class Person {
 
 const person = new Person();
 console.log(person);
+
+// ---
+const Log = (target: any, propertyName: string | Symbol) => {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+};
+
+const Log2 = (target: any, name: string, descriptor: PropertyDescriptor) => {
+  console.log("Accessor Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+};
+
+const Log3 = (target: any, name: string | Symbol, descriptor: PropertyDescriptor)=>{
+  console.log("Method Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+};
+
+const Log4 = (target: any, name: string | Symbol, position: number) => {
+  console.log("Parameter Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+};
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log2
+  setPrice(value: number) {
+    if (value > 0) {
+      this._price = value;
+    } else {
+      throw new Error("Invalid price - should be positive");
+    }
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
