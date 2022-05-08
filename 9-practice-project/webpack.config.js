e98c9d5a36cbce65755de570c570c570ee39c3fe
강명구 <mg5566@hanmail.net>
2022-05-08 22:04:17 +0900

    feat: add webpack config files

--- 9-practice-project/webpack.config.js
+++ 9-practice-project/webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
   devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "."),
+    },
+    compress: true,
+    port: 9000,
+  },
   module: {
     rules: [
       {
@@ -18,5 +22,10 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
   },
 };
