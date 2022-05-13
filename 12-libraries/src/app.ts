57d1102b059b6d648d6cf7bfcbb245ca6f66906f
강명구 <mg5566@hanmail.net>
2022-05-13 17:16:56 +0900

    learn about class validator

--- 12-libraries/src/app.ts
+++ 12-libraries/src/app.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import { plainToClass } from "class-transformer";
 
+import { validate } from "class-validator";
+
 import { Product } from "./product.model";
 
 // const p1 = new Product("A Book", 30);
@@ -11,6 +13,15 @@ const products = [
   { title: "MacBook", price: 290 },
   { title: "A Book", price: 30 },
 ];
+
+const newProd = new Product("", -23);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATE FAIL", errors);
+  } else {
+    console.log("newProduct", newProd.getInformation());
+  }
+});
 
 // const loadedProducts = products.map((prod) => {
 //   return new Product(prod.title, prod.price);
