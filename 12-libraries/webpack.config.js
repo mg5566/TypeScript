a6795d792d1a5d8ad0f75ed624d9ae85227fcdf3
강명구 <mg5566@hanmail.net>
2022-05-13 15:24:39 +0900

    feat: build lodash

--- 12-libraries/webpack.config.js
+++ 12-libraries/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
+const WebpackDevServer = require("webpack-dev-server");
+const webpackConfig = require('./webpack.config.js');
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -18,5 +21,12 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "."),
+    },
+    compress: true,
+    port: 9000,
   },
 };
