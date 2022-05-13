0a4b2e3b5a29234cc46ccea7dcccca0dcc012288
강명구 <mg5566@hanmail.net>
2022-05-13 22:30:25 +0900

    it's a real init webpack setting: perfect

---
+++ 13-practice/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.ts",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "./index.html",
+    }),
+  ],
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: ".",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
