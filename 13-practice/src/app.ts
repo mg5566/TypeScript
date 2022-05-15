de6bf6e37fa536a83c9ec3fd40d4c639812d7523
mg5566 <mg5566@hanmail.net>
2022-05-15 14:57:43 +0900

    13 practice (#13)
    * it's a real init webpack setting: perfect
    
    * feat: add geocoding api
    
    * feat: add google maps typescript package
    
    * feat: add feature of google map redering

---
+++ 13-practice/src/app.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyA9rlMDhBLm5iFqwAwWawD9YQtSNaD51FM";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+const searchHandler = (event: SubmitEvent) => {
+  event.preventDefault();
+  console.log("address", addressInput.value);
+  const enteredAddress = addressInput.value;
+
+  // send address to Google's API
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      console.log(response);
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map")!, {
+        center: coordinates,
+        zoom: 16,
+      });
+      const marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+};
+
+form.addEventListener("submit", searchHandler);
